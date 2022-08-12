0199244d550a7415cc329a9bf7d63c95c7a991d4
Yuri Rodrigues Silva <yrodrisi@emeal.nttdata.com>
2022-08-12 17:05:45 -0300

    hacker-station

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -13,48 +13,48 @@ export class LoginPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  validaLogin(): void {
-    const usuario = "admin"
-    const senha = "123"
-    let usuarioDigitado = (<HTMLInputElement>document.getElementById("usuario"))
-    let senhaDigitada = (<HTMLInputElement>document.getElementById("senha"))
-    let erros = (<HTMLInputElement>document.getElementById("erros"))
-    let btnLogin = (<HTMLInputElement>document.getElementById("btnLogin"))
+  validaLogin(event: any): void {
+    if (event.key === "Enter") {
+      const usuario = "admin"
+      const senha = "123"
+      let usuarioDigitado = (<HTMLInputElement>document.getElementById("usuario"))
+      let senhaDigitada = (<HTMLInputElement>document.getElementById("senha"))
+      let erros = (<HTMLInputElement>document.getElementById("erros"))
+      let btnLogin = (<HTMLInputElement>document.getElementById("btnLogin"))
 
-    console.log(`Usuario digitado:${usuarioDigitado.value}\nSenha digitada:${senhaDigitada.value}`)
-    if(usuarioDigitado.value == usuario && senhaDigitada.value == senha){
-      console.log("\nAcesso permitido!")
-      this.router.navigate(['/home'])
-    }else if(usuarioDigitado.value == "" || senhaDigitada.value == ""){
-      erros.className = "errosAtivo"
-      erros.innerHTML = `
-        <p>TODOS OS CAMPOS SÃO OBRIGATÓRIOS</p>
-      `
-      window.alert("Todos os campos são obrigatórios.")
-    }else{
-      console.log("\nAcesso NEGADO!")
-      erros.className = "errosAtivo"
-      erros.innerHTML = `
-        <p>Usuario ou senha inválidos. Tente novamente!</p>
-        <p>Não possui uma conta?<a href="">Clique aqui para cadastrar-se</a></p>
-        <br>
-        <details>
-          <summary>Clique aqui para ver detalhes do erro</summary>
-          <p>
-            Os dados a seguir <strong>não estão cadastrados no sistema:</strong>
-            <br>
-            Usuario digitado: ${usuarioDigitado.value} 
-            <br>
-            Senha digitada: ${senhaDigitada.value}
-          </p>
-        </details>
-        
-      `
+      console.log(`Usuario digitado:${usuarioDigitado.value}\nSenha digitada:${senhaDigitada.value}`)
+      if(usuarioDigitado.value == usuario && senhaDigitada.value == senha){
+        console.log("\nAcesso permitido!")
+        this.router.navigate(['/home'])
+      }else if(usuarioDigitado.value == "" || senhaDigitada.value == ""){
+        erros.className = "errosAtivo"
+        erros.innerHTML = `
+          <p>TODOS OS CAMPOS SÃO OBRIGATÓRIOS</p>
+        `
+        window.alert("Todos os campos são obrigatórios.")
+      }else{
+        console.log("\nAcesso NEGADO!")
+        erros.className = "errosAtivo"
+        erros.innerHTML = `
+          <p>Usuario ou senha inválidos. Tente novamente!</p>
+          <p>Não possui uma conta?<a href="">Clique aqui para cadastrar-se</a></p>
+          <br>
+          <details>
+            <summary>Clique aqui para ver detalhes do erro</summary>
+            <p>
+              Os dados a seguir <strong>não estão cadastrados no sistema:</strong>
+              <br>
+              Usuario digitado: ${usuarioDigitado.value} 
+              <br>
+              Senha digitada: ${senhaDigitada.value}
+            </p>
+          </details>
+          
+        `
+      }
+
+      usuarioDigitado.value = ""
+      senhaDigitada.value = ""
     }
-
-    usuarioDigitado.value = ""
-    senhaDigitada.value = ""
   }
-
-  
 }
